19d4124f57bffe54e8fc88c9fcff10c3a606defd
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-03 16:57:14 -0500

    Returned early if the post type is not post.
    The first conditional is a "no go" condition, where we
    do not want to process our business logic if it is not a "post"
    post type.  We do not want "no go" conditions to continue to flow
    as that is a flawed design.  Rather, when we find one, we want to
    immediately stop and bail out.
    
    The other benefits are:
    
    1. It communicates the intent that if this is not a "post" post type,
    then bail out.
    2. Only do the work (business logic) when it's a "post" post type.
    3. It unwraps the business logic, moving it to the left. That makes it
    more readable as there are less levels (depths) of indentation.
    4. It prevents someone from misunderstanding what this "no go" is.
    Imagine that someone comes along and puts code after the if code block.
    That would cause unexpected behavior.
    
    @see https://torquemag.io/2018/04/code-review-part-1-fixing-design-flaw-return-early-strategy/

--- jsforwp-example-blocks/lib/block-templates.php
+++ jsforwp-example-blocks/lib/block-templates.php
@@ -3,22 +3,25 @@
 //add_filter( 'register_post_type_args', 'jsforwpblocks_templates', 20, 2 );
 function jsforwpblocks_templates( $args, $post_type ) {
 
-  if ( 'post' === $post_type ) {
-    $args['template_lock'] = true;
-    $args['template'] = [
-      [
-        'core/image', [
-          'align' => 'left',
-        ]
-      ],
-      [
-        'core/paragraph', [
-          'placeholder' => 'The only thing you can add',
-        ]
-      ]
-    ];
-  }
+	if ( 'post' !== $post_type ) {
+		return $args;
+	}
 
-  return $args;
+	$args['template_lock'] = true;
+	$args['template']      = [
+		[
+			'core/image',
+			[
+				'align' => 'left',
+			],
+		],
+		[
+			'core/paragraph',
+			[
+				'placeholder' => 'The only thing you can add',
+			],
+		],
+	];
 
+	return $args;
 }
