00294bcf0360d033e17fbc193b327ec24344e2a4
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-03 16:34:15 -0500

    Removed the unnecessary parentheses in the include.
    The `include` and `require` are not functions, but rather are
    special language constructs (statement).  We do not invoke them using
    parentheses.
    
    Instead, just call the statement followed by the file path:
    
    include __DIR__ . '/path/to/filename.php';
    
    -------------------
    Per the PHP Manual:
    -------------------
    
    >Because include is a special language construct, parentheses are not
    >needed around its argument. Take care when comparing return value.
    
    @see http://php.net/manual/en/function.include.php

--- jsforwp-example-blocks/jsforwp-example-blocks.php
+++ jsforwp-example-blocks/jsforwp-example-blocks.php
@@ -23,11 +23,11 @@
 defined('ABSPATH') || exit;
 
 // Enqueue JS and CSS
-include( plugin_dir_path( __FILE__ ) . 'lib/enqueue-scripts.php');
+include plugin_dir_path( __FILE__ ) . 'lib/enqueue-scripts.php';
 // Register meta boxes
-include( plugin_dir_path( __FILE__ ) . 'lib/meta-boxes.php');
+include plugin_dir_path( __FILE__ ) . 'lib/meta-boxes.php';
 // Block Templates
-include( plugin_dir_path( __FILE__ ) . 'lib/block-templates.php');
+include plugin_dir_path( __FILE__ ) . 'lib/block-templates.php';
 
 // Dynamic Blocks
-include( plugin_dir_path( __FILE__ ) . 'blocks/12-dynamic/index.php');
+include plugin_dir_path( __FILE__ ) . 'blocks/12-dynamic/index.php';
