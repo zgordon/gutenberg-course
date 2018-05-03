1eadff166faefc64cf176ebcdfe1fd355ec85c83
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-03 18:30:08 -0500

    Improved the dynamic block PHP code.
    1. Inverted the Gutenberg checker to act as a guard clause.  If
    not loaded, then the file bails out.  By moving it to the top,
    we are communicating the intent that this file should not run
    when Gutenberg is not installed.
    
    2. Moved the registration function up as it happens first in the
    sequence.  The rendering function is a callback that happens later.

--- jsforwp-example-blocks/blocks/12-dynamic/index.php
+++ jsforwp-example-blocks/blocks/12-dynamic/index.php
@@ -2,43 +2,49 @@
 
 namespace Gutenberg_Courses\Example_Block\Blocks\Dynamic;
 
+// Only load if Gutenberg is available.
+if ( ! function_exists( 'register_block_type' ) ) {
+	return;
+}
+
+add_action( 'init', 'register_dynamic_block' );
 /**
- * Server rendering for /blocks/examples/12-dynamic
+ * Register the dynamic block.
+ *
+ * @since 2.1.0
+ *
+ * @return void
  */
-function render_dynamic_block( $attributes ) {
-
-    $recent_posts = wp_get_recent_posts( [
-        'numberposts' => 3,
-        'post_status' => 'publish',
-    ] );
-
-    if ( 0 === count( $recent_posts ) ) {
-        return '<p>No posts</p>';
-    }
-    $markup = '<ul>';
-
-    foreach( $recent_posts as $post ) {
-        $post_id = $post['ID'];
-        $markup .= sprintf(
-            '<li><a href="%1$s">%2$s</a></li>',
-            esc_url( get_permalink( $post_id ) ),
-            esc_html( get_the_title( $post_id ) )
-        );
-    }
-    $markup .= '<ul>';
-
-    return $markup;
-
-}
-
-// Make sure that Gutenberg is available
-if ( function_exists( 'register_block_type' ) ) {
-	add_action( 'init', 'register_the_blocks' );
-}
-
-function register_the_blocks() {
+function register_dynamic_block() {
   // Hook server side rendering into render callback
   register_block_type( 'jsforwpblocks/dynamic', [
       'render_callback' => __NAMESPACE__ . '\render_dynamic_block',
   ] );
+}
+
+/**
+ * Server rendering for /blocks/examples/12-dynamic
+ */
+function render_dynamic_block( $attributes ) {
+	$recent_posts = wp_get_recent_posts( [
+		'numberposts' => 3,
+		'post_status' => 'publish',
+	] );
+
+	if ( 0 === count( $recent_posts ) ) {
+		return '<p>No posts</p>';
+	}
+	$markup = '<ul>';
+
+	foreach( $recent_posts as $post ) {
+		$post_id = $post['ID'];
+		$markup .= sprintf(
+			'<li><a href="%1$s">%2$s</a></li>',
+			esc_url( get_permalink( $post_id ) ),
+			esc_html( get_the_title( $post_id ) )
+		);
+	}
+	$markup .= '<ul>';
+
+	return $markup;
 }
