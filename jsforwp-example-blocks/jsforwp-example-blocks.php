89c5454c812e8aa8c3cc4dd801b28d032a48ad2a
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-03 17:59:37 -0500

    Refactored path to plugin's dir and URI.
    The assets need the plugin's root directory as well as it's URI.
    The previous design was slower.  Why?  It had to call `plugins_url()`
    and `plugin_dir_path()` 4 times each.  These functions call other
    functions.
    
    As we previously discussed, the function `plugin_dir_path()` calls 4
    other functions.  Let's do the math:
    
    4 times it runs x 5 functions = 20 total functions being called.
    
    How about `plugins_url()`?  This function is expensive as it calls:
    
    - wp_normalize_path() 3 times
    - strpos()
    - set_url_scheme()
    - plugin_basename()
    - dirname()
    - is_string() 2 times
    - ltrim() 2 times
    - apply_filters()
    - and makes 8 assignment operations
    
    That's slow.
    
    We can improve the performance by using 2 functions in the bootstrap
    file of the plugin:
    
    - _get_plugin_url()
    - _get_plugin_directory()
    
    These functions are internal to the plugin and, therefore, start with
    an underscore to communicate that intent.
    
    This strategy does add 2 more functions and as well as a static
    variable and memory location. But it is much faster than using the
    old approach, as discussed above.

--- jsforwp-example-blocks/jsforwp-example-blocks.php
+++ jsforwp-example-blocks/jsforwp-example-blocks.php
@@ -24,6 +24,38 @@ namespace Gutenberg_Courses\Example_Blocks;
 //  Exit if accessed directly.
 defined('ABSPATH') || exit;
 
+/**
+ * Gets this plugin's absolute directory path.
+ *
+ * @since  1.0.0
+ * @ignore
+ * @access private
+ *
+ * @return string
+ */
+function _get_plugin_directory() {
+	return __DIR__;
+}
+
+/**
+ * Gets this plugin's URL.
+ *
+ * @since  1.0.0
+ * @ignore
+ * @access private
+ *
+ * @return string
+ */
+function _get_plugin_url() {
+	static $plugin_url;
+
+	if ( empty( $plugin_url ) ) {
+		$plugin_url = plugins_url( null, __FILE__ );
+	}
+
+	return $plugin_url;
+}
+
 // Enqueue JS and CSS
 include __DIR__ . '/lib/enqueue-scripts.php';
 
--- jsforwp-example-blocks/lib/enqueue-scripts.php
+++ jsforwp-example-blocks/lib/enqueue-scripts.php
@@ -10,23 +10,23 @@ function enqueue_block_editor_assets()
 {
 
     // Make paths variables so we don't write em twice ;)
-    $blockPath = '../assets/js/editor.blocks.js';
-    $editorStylePath = '../assets/css/blocks.editor.css';
+    $blockPath = '/assets/js/editor.blocks.js';
+    $editorStylePath = '/assets/css/blocks.editor.css';
 
     // Enqueue the bundled block JS file
     wp_enqueue_script(
         'jsforwp-blocks-js',
-        plugins_url( $blockPath, __FILE__ ),
+	    _get_plugin_url() . $blockPath,
         [ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components' ],
-        filemtime( plugin_dir_path(__FILE__) . $blockPath )
+        filemtime( _get_plugin_directory() . $blockPath )
     );
 
     // Enqueue optional editor only styles
     wp_enqueue_style(
         'jsforwp-blocks-editor-css',
-        plugins_url( $editorStylePath, __FILE__),
+	    _get_plugin_url() . $editorStylePath,
         [ 'wp-blocks' ],
-        filemtime( plugin_dir_path( __FILE__ ) . $editorStylePath )
+        filemtime( _get_plugin_directory() . $editorStylePath )
     );
 
 }
@@ -38,25 +38,25 @@ add_action('enqueue_block_assets', __NAMESPACE__ . '\enqueue_assets');
 function enqueue_assets()
 {
     // Make paths variables so we don't write em twice ;)
-    $blockPath = '../assets/js/frontend.blocks.js';
-    $stylePath = '../assets/css/blocks.style.css';
+    $blockPath = '/assets/js/frontend.blocks.js';
+    $stylePath = '/assets/css/blocks.style.css';
 
     if( !is_admin() ) {
         // Enqueue the bundled block JS file
         wp_enqueue_script(
             'jsforwp-blocks-frontend',
-            plugins_url( $blockPath, __FILE__ ),
+	        _get_plugin_url() . $blockPath,
             [],
-            filemtime( plugin_dir_path(__FILE__) . $blockPath )
+            filemtime( _get_plugin_directory() . $blockPath )
         );
     }
 
     // Enqueue frontend and editor block styles
     wp_enqueue_style(
         'jsforwp-blocks',
-        plugins_url($stylePath, __FILE__),
+	    _get_plugin_url() . $stylePath,
         [ 'wp-blocks' ],
-        filemtime(plugin_dir_path(__FILE__) . $stylePath )
+        filemtime(_get_plugin_directory() . $stylePath )
     );
 
 }
