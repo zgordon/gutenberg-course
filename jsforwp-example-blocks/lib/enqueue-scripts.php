06e0bde2c4fdc3c3301ef0957db9660ce36bf9d1
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-03 19:02:05 -0500

    Renamed variables to comply with WPCS.
    The variable naming was inconsistent.  In some places it complies
    with WPCS by using the underscores.  In the enqueuing file, it
    used camelCase.

--- jsforwp-example-blocks/lib/enqueue-scripts.php
+++ jsforwp-example-blocks/lib/enqueue-scripts.php
@@ -8,23 +8,23 @@ add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\enqueue_block_edito
  */
 function enqueue_block_editor_assets() {
 	// Make paths variables so we don't write em twice ;)
-	$blockPath       = '/assets/js/editor.blocks.js';
-	$editorStylePath = '/assets/css/blocks.editor.css';
+	$block_path = '/assets/js/editor.blocks.js';
+	$style_path = '/assets/css/blocks.editor.css';
 
 	// Enqueue the bundled block JS file
 	wp_enqueue_script(
 		'jsforwp-blocks-js',
-		_get_plugin_url() . $blockPath,
+		_get_plugin_url() . $block_path,
 		[ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components' ],
-		filemtime( _get_plugin_directory() . $blockPath )
+		filemtime( _get_plugin_directory() . $block_path )
 	);
 
 	// Enqueue optional editor only styles
 	wp_enqueue_style(
 		'jsforwp-blocks-editor-css',
-		_get_plugin_url() . $editorStylePath,
+		_get_plugin_url() . $style_path,
 		[ 'wp-blocks' ],
-		filemtime( _get_plugin_directory() . $editorStylePath )
+		filemtime( _get_plugin_directory() . $style_path )
 	);
 }
 
@@ -33,12 +33,12 @@ add_action( 'enqueue_block_assets', __NAMESPACE__ . '\enqueue_assets' );
  * Enqueue front end and editor JavaScript and CSS assets.
  */
 function enqueue_assets() {
-	$stylePath = '/assets/css/blocks.style.css';
+	$style_path = '/assets/css/blocks.style.css';
 	wp_enqueue_style(
 		'jsforwp-blocks',
-		_get_plugin_url() . $stylePath,
+		_get_plugin_url() . $style_path,
 		[ 'wp-blocks' ],
-		filemtime( _get_plugin_directory() . $stylePath )
+		filemtime( _get_plugin_directory() . $style_path )
 	);
 }
 
@@ -53,11 +53,11 @@ function enqueue_frontend_assets() {
 		return;
 	}
 
-	$blockPath = '/assets/js/frontend.blocks.js';
+	$block_path = '/assets/js/frontend.blocks.js';
 	wp_enqueue_script(
 		'jsforwp-blocks-frontend',
-		_get_plugin_url() . $blockPath,
+		_get_plugin_url() . $block_path,
 		[],
-		filemtime( _get_plugin_directory() . $blockPath )
+		filemtime( _get_plugin_directory() . $block_path )
 	);
 }
