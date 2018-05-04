41d233654c2d9b8cbb5e69b5118168114bd60bb6
hellofromtonya <hellofromtonya@knowthecode.io>
2018-05-03 19:03:49 -0500

    Eliminated the variable assignment.
    This change eliminates PHP having to:
    
    1. Create the variable name in the symbols table.
    2. Bind the array's memory location to that variable.
    3. Look up the value when passing it to `register_meta()`.
    
    The results:
    
    1. Less code.
    2. Runs a tiny bit faster.

--- jsforwp-example-blocks/lib/meta-boxes.php
+++ jsforwp-example-blocks/lib/meta-boxes.php
@@ -7,11 +7,12 @@ add_action( 'init', __NAMESPACE__ . '\register_meta_fields' );
  * Registering meta fields for block attributes that use meta storage
  */
 function register_meta_fields() {
-	$args = [
-		'type'         => 'string',
-		'single'       => true,
-		'show_in_rest' => true,
-	];
-
-	register_meta( 'post', 'jsforwpblocks_gb_metabox', $args );
+	register_meta(
+		'post',
+		'jsforwpblocks_gb_metabox',
+		[
+			'type'         => 'string',
+			'single'       => true,
+			'show_in_rest' => true,
+		] );
 }
